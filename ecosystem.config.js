5beec18712766d1008633c96a7c0a7a51a883bbb
Alessandro D'Antoni <alexdant91@gmail.com>
2021-02-25 08:32:24 +0100

    Admin ui scaffolded

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,5 +25,17 @@ module.exports = {
         NODE_ENV: "production",
       },
     },
+    {
+      name: "node-mvc-admin",
+      script: "npm",
+      args: "run start:ui:admin",
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
   ]
 }
