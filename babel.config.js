d74814b39c4de32dcfb517cf1509fba9da3a1c48
Alessandro D'Antoni <alexdant91@gmail.com>
2021-03-13 17:04:07 +0100

    Core updates

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,22 @@
 module.exports = {
   presets: [
-    "@babel/preset-env", "minify"
+    "@babel/preset-env",
+    "minify"
   ],
   plugins: [
     "@babel/plugin-syntax-class-properties",
     "@babel/plugin-proposal-class-properties",
     ["@babel/plugin-transform-runtime", { "regenerator": true }]
   ],
+  ignore: [
+    ".env",
+    "test",
+    "**/*.test.js",
+    "babel.config.js",
+    "ecosystem.config.js",
+    "dump.rdb",
+    "node_modules",
+    "resource",
+    "database",
+  ]
 }
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,39 +3,16 @@ require('dotenv').config();
 module.exports = {
   apps: [
     {
-      name: process.env.APP_NAME,
-      script: "./app.js",
+      name: "app-server",
+      script: "npm",
+      args: "run start:server",
       exec_mode: "cluster",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
     },
     {
-      name: "node-mvc-ui",
+      name: "app-ui",
       script: "npm",
       args: "run start:ui",
       exec_mode: "fork",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
-    },
-    {
-      name: "node-mvc-admin",
-      script: "npm",
-      args: "run start:ui:admin",
-      exec_mode: "fork",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
     },
   ]
 }
